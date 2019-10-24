eee35cbfbbcc2210d021a4a4a98656ac6f5a8817
bigmaxattack37 <bigmaxmarcus@gmail.com>
2019-10-24 12:06:20 -0500

    started a projectile

--- game.py
+++ game.py
@@ -20,14 +20,24 @@ class Player:
         self.jump_vel = 5
         self.highest_jump_vel = self.jump_vel
 
-        self.right = False
-        self.left = False
+        self.direction = 0
         self.animation_frame = 0
 
+class Projectile:
+    def __init__(self,x,y,radius,color,facing):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.vel = 10 * facing
+
+    def draw(self,win):
+        pygame.draw.circle(win,self.color,(self.x,self.y),self.radius)
 
 
 player = Player(300,410,64,64)
-
+bullets = []
 
 run = True
 while run:
@@ -37,14 +47,25 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    for i in bullets:
+        if i.x < 500 and i.x > 0:
+            bullet.x += bullet.vel
+        else:
+            bullets.pop(bullets.index(i))
 
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        if len(bullets) < 10:
+            bullets.append(Projectile(player.x + player.width//2,player.y+player.height//2))
+
     if keys[pygame.K_LEFT] and player.x > player.vel:
         player.x -= player.vel
+        player.direction = -1
     if keys[pygame.K_RIGHT] and player.x < 500-player.vel-player.width:
         player.x += player.vel
+        player.direction = 1
     if not player.is_jump:
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_UP]:
             player.is_jump = True
     else:
         if player.jump_vel >= -player.highest_jump_vel:
