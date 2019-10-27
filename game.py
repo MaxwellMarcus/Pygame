a710f3af69abf156f185affc8fa3c2e80453647b
MaxwellMarcus <bigmaxmarcus@gmail.com>
2019-10-27 17:06:54 -0500

    An animation is working

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import pygame
 pygame.init()
 
-screen_width = 500
-screen_height = 500
+screen_width = 1000
+screen_height = 700
 
 win = pygame.display.set_mode((screen_width,screen_height))
 
@@ -17,11 +17,33 @@ class Player:
         self.vel = 5
 
         self.is_jump = False
-        self.jump_vel = 5
+        self.jump_vel = 15
         self.highest_jump_vel = self.jump_vel
 
-        self.direction = 0
+        self.direction = 1
         self.animation_frame = 0
+
+        self.left = [pygame.image.load('Stick Man\\Left 1.png'),pygame.image.load('Stick Man\\Left 2.png'),pygame.image.load('Stick Man\\Left 3.png'),pygame.image.load('Stick Man\\Left 4.png'),pygame.image.load('Stick Man\\Left 5.png'),pygame.image.load('Stick Man\\Left 6.png'),pygame.image.load('Stick Man\\Left 7.png')]
+        self.right = [pygame.image.load('Stick Man\\Right 1.png'),pygame.image.load('Stick Man\\Right 2.png'),pygame.image.load('Stick Man\\Right 3.png'),pygame.image.load('Stick Man\\Right 4.png'),pygame.image.load('Stick Man\\Right 5.png'),pygame.image.load('Stick Man\\Right 6.png'),pygame.image.load('Stick Man\\Right 7.png')]
+        for i in self.right:
+            size = i.get_rect().size
+            x = size[0]
+            y = size[1]
+            self.right[self.right.index(i)] = pygame.transform.scale(i,(x//20,y//20))
+        for i in self.left:
+            size = i.get_rect().size
+            x = size[0]
+            y = size[1]
+            self.left[self.left.index(i)] = pygame.transform.scale(i,(x//20,y//20))
+
+    def render(self,win):
+        if self.animation_frame > 20:
+            self.animation_frame = 0
+        if self.direction == 1:
+            win.blit(self.right[self.animation_frame//3], (self.x,self.y))
+        if self.direction == -1:
+            win.blit(self.left[self.animation_frame//3], (self.x,self.y))
+
 
 class Projectile:
     def __init__(self,x,y,radius,color,facing):
@@ -33,37 +55,42 @@ class Projectile:
         self.vel = 10 * facing
 
     def draw(self,win):
-        pygame.draw.circle(win,self.color,(self.x,self.y),self.radius)
+        pygame.draw.circle(win,self.color,(self.x,self.y),self.radius,0)
 
 
-player = Player(300,410,64,64)
+player = Player(300,410,25,50)
 bullets = []
 
 run = True
 while run:
     pygame.time.delay(50)
 
+    win.fill((255,0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     for i in bullets:
-        if i.x < 500 and i.x > 0:
-            bullet.x += bullet.vel
+        if i.x < screen_width and i.x > 0:
+            i.x += i.vel
+            i.draw(win)
         else:
             bullets.pop(bullets.index(i))
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
-        if len(bullets) < 10:
-            bullets.append(Projectile(player.x + player.width//2,player.y+player.height//2))
+        if len(bullets) < 100:
+            bullets.append(Projectile(round(player.x + player.width//2),round(player.y+player.height//2),6,(255,255,255),player.direction))
 
     if keys[pygame.K_LEFT] and player.x > player.vel:
         player.x -= player.vel
         player.direction = -1
-    if keys[pygame.K_RIGHT] and player.x < 500-player.vel-player.width:
+        player.animation_frame += 1
+    if keys[pygame.K_RIGHT] and player.x < screen_width-player.vel-player.width:
         player.x += player.vel
         player.direction = 1
+        player.animation_frame += 1
     if not player.is_jump:
         if keys[pygame.K_UP]:
             player.is_jump = True
@@ -80,8 +107,7 @@ while run:
             player.jump_vel = player.highest_jump_vel
 
 
-    win.fill((0,0,0))
-    pygame.draw.rect(win, (255,0,0),(player.x,player.y,player.width,player.height))
+    player.render(win)
     pygame.display.update()
 
 pygame.quit()
